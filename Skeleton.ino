828fe258196f89c5c88659179386cda647f2b1fc
MARS <md-asifur-rahman.saikot@stud.hshl.de>
2025-09-23 21:01:06 +0200

    Create Skeleton.ino

---
+++ Skeleton.ino
@@ -0,0 +1,23 @@
+/*
+ * Author: [Your Name Here]
+ * Iteration: 1
+ * Date: DD/MM/YYYY
+ * Description: [Briefly describe what this iteration does]
+ * Notes: Tested on Generic Arduino-Compatible Board
+ */
+
+void setup() {
+  // Initialize serial communication or pins here
+  Serial.begin(9600);
+  
+  // Example: pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // Your code goes here
+  // Example: Blink built-in LED
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(500);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(500);
+}
